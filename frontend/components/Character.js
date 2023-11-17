f4d5ffed9889048c9b0a8e8ddb6084526ffae7cd
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2023-11-17 14:23:45 -0500

    characterTest

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -4,19 +4,23 @@ function Character( { character } ) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   const [showHomeworld, setShowHomeworld] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
-  const toggleHomeworld = () => {
+  const toggleHomeworld = (event) => {
+    event.stopPropagation();
     setShowHomeworld(!showHomeworld);
   }
 
   return (
-    <div>
+    <div className="character-card" onClick={toggleHomeworld}>
       {/* Use the same markup with the same attributes as in the mock */}
-      <h3>{character.name}</h3>
+      <h3 className="character-name">{character.name}</h3>
 
-      {/* Conditionally render the homeworld name */}
-      <p onClick={toggleHomeworld}>Planet: {showHomeworld ? character.homeworld.name : 'Click to show'}</p>
+      {/* Conditionally render the entire p tag */}
+      {showHomeworld && (
+        <p className="character-planet">
+          Planet: {character.homeworld.name}
+        </p>
+      )}
     </div>
   );
 }
-
 export default Character;
